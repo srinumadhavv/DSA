acc13b1d73d3e68964124116df6085361d464787
Madhav <srinumadhavv@gmail.com>
2023-06-07 22:37:51 +0530

    7jun

---
+++ Arrays2/mergeOverlappingSubIntervals.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<n;i++){
+            if(ans.size()==0||ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};

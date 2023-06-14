662a44b2718445d0dfef15f21254c0984a25e177
Madhav <srinumadhavv@gmail.com>
2023-06-14 16:20:19 +0530

    14jun

---
+++ Linkedlist2/reverseKgroups.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL || k==1) return head;
+        int count=0;
+        ListNode *dummy = new ListNode();
+        dummy->next=head;
+        ListNode *pre = dummy, *curr = dummy, *nex=dummy;
+        while(curr->next!=NULL){
+            count++;
+            curr=curr->next;
+        }
+        while(count>=k){
+            curr=pre->next;
+            nex=curr->next;
+            for(int i=1;i<k;i++){
+                curr->next=nex->next;
+                nex->next=pre->next;
+                pre->next=nex;
+                nex=curr->next;
+            }
+            pre=curr;
+            count-=k;
+        }
+        return dummy->next;
+    }
+};
+Console
